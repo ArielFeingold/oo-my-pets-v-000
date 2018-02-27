ec1372bad2e613f78cc156c388643d1bde109c65
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-27 17:41:31 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -53,11 +53,14 @@ class Owner
   end
 
   def sell_pets
-
     @pets[:dogs].each {|dog| dog.mood = "nervous"}
     @pets[:cats].each {|cat| cat.mood = "nervous"}
     @pets[:fishes].each {|fish| fish.mood = "nervous"}
     @pets = []
+  end
+
+  def list_pets
+    
   end
 
 end
